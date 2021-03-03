e3275c687cdd5b9fde5c32934453f5cc7bad829d
@slovacus <slovacus@gmail.com>
2021-03-03 14:29:22 -0500

    perf: validate news files with prettiser (#15)

--- .github/linters/prettier.config.js
+++ .github/linters/prettier.config.js
@@ -8,7 +8,21 @@ module.exports = {
   singleQuote: true,
   overrides: [
     {
-      files: '*',
+      files: '{*.js?(on),*.y?(a)ml,.*.js?(on),.*.y?(a)ml,*.md,.prettierrc,.stylelintrc,.babelrc}',
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: '{**/.vscode/*.json,**/tsconfig.json,**/tsconfig.*.json,.stylelintrc}',
+      options: {
+        parser: 'json',
+        quoteProps: 'preserve',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.md',
       options: {
         parser: 'markdown',
         printWidth: 100,
@@ -16,6 +30,29 @@ module.exports = {
         semi: false,
         trailingComma: 'none',
       },
-    }
+    },
+    {
+      files: '*.mdx',
+      options: {
+        printWidth: 100,
+        proseWrap: 'never',
+        semi: false,
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: '*.{sass,scss}',
+      options: {
+        parser: 'scss',
+      },
+    },
+    {
+      files: '*.less',
+      options: {
+        tabWidth: 4,
+        printWidth: 80,
+        parser: 'less',
+      },
+    },
   ],
 }
